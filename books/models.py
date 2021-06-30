cf46acf425d4eb59fac6789049fcbfab9929b204
Bruno Garcia <bruno@garciaechegaray.com>
2021-06-30 18:51:41 +0200

    feat: add reviews app

--- books/models.py
+++ books/models.py
@@ -1,14 +1,17 @@
 import uuid
+
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
 
 class Book(models.Model):
+    objects = None
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False
-        )
+    )
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -18,3 +21,20 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
+
+
+class Review(models.Model):
+    objects = None
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
